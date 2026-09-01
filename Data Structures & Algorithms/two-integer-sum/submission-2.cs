d8d9349ba4c909e633e00597bf77206ac675e478
Bisma Aslam <144362416+Bisma-Aslam@users.noreply.github.com>
2026-09-01 14:51:23 +0500

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cs
@@ -0,0 +1,15 @@
+public class Solution{
+    public int[] TwoSum(int[] nums,int target){
+        Dictionary<int,int> prev=new Dictionary<int,int>();
+        for(int i=0; i<nums.Length;i++){
+            var diff=target-nums[i];
+            if(prev.ContainsKey(diff)){
+                return new int[]{prev[diff],i};
+            }
+            prev[nums[i]]=i;
+        
+        }
+        return null;
+
+    }
+}
