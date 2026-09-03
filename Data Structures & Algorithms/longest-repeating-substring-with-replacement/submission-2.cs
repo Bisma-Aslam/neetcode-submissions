5b38d8ca7b805164db626d6520c6e64d5290079a
Bisma Aslam <144362416+Bisma-Aslam@users.noreply.github.com>
2026-09-03 14:20:11 +0500

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cs
@@ -0,0 +1,24 @@
+public class Solution{
+    public int CharacterReplacement(string s,int k){
+        int maxF=0;
+        int l=0;
+        int res=0;
+        Dictionary<char,int> count=new Dictionary<char,int>();
+        for(int r=0;r<s.Length;r++){
+            if(count.ContainsKey(s[r])){
+                count[s[r]]++;
+            }else{
+                count[s[r]]=1;
+            }
+            maxF=Math.Max(maxF,count[s[r]]);
+            while((r-l+1)-maxF>k){
+                count[s[l]]--;
+                l++;
+            }
+            res=Math.Max(res,(r-l+1));
+
+        }
+        return res;
+        
+    }
+}
